54b130349b9e681c654d43c83d69435ca0b3fc80
jentlAntheunis <113892951+jentlAntheunis@users.noreply.github.com>
2024-01-10 11:43:09 +0100

    deleted bin folder

--- api/obj/Debug/net6.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net6.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4e06e9330466aed9b74df4f32c514b42acb9a7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
