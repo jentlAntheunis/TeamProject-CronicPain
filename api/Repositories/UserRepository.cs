3e1108cca98d0b2411bfdb90239cecec9838e496
liendejong <liendejong03@gmail.com>
2024-01-11 14:08:29 +0100

    Commit before review other code

--- api/Repositories/UserRepository.cs
+++ api/Repositories/UserRepository.cs
@@ -1,1 +1,52 @@
+using Pebbles.Models;
+using Pebbles.Context;
+using Microsoft.EntityFrameworkCore;
 
+namespace Pebbles.Repositories;
+
+public interface IUserRepository
+{
+    Task<List<User>> GetAllUsersAsync();
+    Task<User> GetUserByIdAsync(Guid id);
+    Task<User> CreateUserAsync(User user);
+    Task<User> UpdateUserAsync(User user);
+    Task DeleteUserAsync(Guid id);
+
+}
+
+public class UserRepository : IUserRepository
+{
+    private readonly PebblesContext _context;
+
+    public UserRepository(PebblesContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<User>> GetAllUsersAsync() => await _context.User.ToListAsync();
+
+    public async Task<User> GetUserByIdAsync(Guid id) => await _context.User.FirstOrDefaultAsync(u => u.Id == id);
+
+    public async Task<User> CreateUserAsync(User user)
+    {
+        await _context.User.AddAsync(user);
+        await _context.SaveChangesAsync();
+        return user;
+    }
+
+    public async Task<User> UpdateUserAsync(User user)
+    {
+        _context.User.Update(user);
+        await _context.SaveChangesAsync();
+        return user;
+    }
+
+    public async Task DeleteUserAsync(Guid id)
+    {
+        var user = await GetUserByIdAsync(id);
+        _context.User.Remove(user);
+        await _context.SaveChangesAsync();
+    }
+
+
+}
---
+++ api/Services/UserService.cs
@@ -0,0 +1,41 @@
+using Pebbles.Models;
+using Pebbles.Repositories;
+
+namespace Pebbles.Services;
+
+public interface IUserService
+{
+    Task<List<User>> GetUsersAsync();
+    Task<User> GetUserAsync(Guid id);
+    Task<User> AddUserAsync(User user);
+    Task<User> UpdateUserAsync(User user);
+    Task DeleteUserAsync(Guid id);
+
+    Task CheckIfUserExistsAsync(Guid id);
+}
+
+public class UserService : IUserService
+{
+    private readonly IUserRepository _userRepository;
+
+    public UserService(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    public async Task<List<User>> GetUsersAsync() => await _userRepository.GetUsersAsync();
+    public async Task<User> GetUserAsync(Guid id) => await _userRepository.GetUserAsync(id);
+    public async Task<User> AddUserAsync(User user) => await _userRepository.AddUserAsync(user);
+    public async Task<User> UpdateUserAsync(User user) => await _userRepository.UpdateUserAsync(user);
+    public async Task DeleteUserAsync(Guid id) => await _userRepository.DeleteUserAsync(id);
+
+    public async Task CheckIfUserExistsAsync(Guid id)
+    {
+        var user = await GetUserAsync(id);
+        if(user == null)
+        {
+            throw new Exception("User does not exist");
+        }
+        return (user.Email, user.FirstName, user.LastName);
+    }
+}
