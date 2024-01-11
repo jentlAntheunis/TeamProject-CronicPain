e5f090ea9369abd105fdcde36c78f14443c5c057
Thibault <thibault.feraux@hotmail.com>
2024-01-11 22:53:06 +0100

    feat:

--- app/src/components/app/auth/AuthContainer.jsx
+++ app/src/components/app/auth/AuthContainer.jsx
@@ -1,14 +1,15 @@
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { AuthRoutes } from "../../../core/config/routes";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../../core/services/firebase";
 
 const AuthContainer = () => {
   console.log("test");
 
   const location = useLocation();
-  // TODO: Check auth status
-  const auth = false;
+  const [user] = useAuthState(auth);
 
-  if (!auth) {
+  if (!user) {
     return (
       <Navigate to={AuthRoutes.Login} state={{ from: location }} replace />
     );
--- app/src/components/app/auth/OnboardingLayout.jsx
+++ app/src/components/app/auth/OnboardingLayout.jsx
@@ -1,11 +1,61 @@
+import { isSignInWithEmailLink } from "firebase/auth";
+import { useEffect, useState } from "react";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { auth } from "../../../core/services/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import CenteredInfo from "../../ui/Centered/CenteredInfo";
+import { toast } from "react-toastify";
 
 const OnboardingLayout = () => {
   const location = useLocation();
+  const search = location.search;
   // TODO: Check auth status
-  const auth = false;
+  const [user] = useAuthState(auth);
 
-  if (!auth) {
+  // useStates
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  // useEffect(() => {
+  //   if (!user) {
+  //     if (isSignInWithEmailLink(auth, window.location.href)) {
+  //       let email = window.localStorage.getItem("emailForSignIn");
+  //       if (!email) {
+  //         // ask user for email
+  //         email = window.prompt("Geef uw e-mailadres voor bevestiging");
+  //       }
+  //       // sign in user
+  //       setLoading(true);
+  //       auth
+  //         .signInWithEmailLink(email, window.location.href)
+  //         .then(() => {
+  //           window.localStorage.removeItem("emailForSignIn");
+  //           console.log("signed in");
+  //           setLoading(false);
+  //           setError(false);
+  //         })
+  //         .catch((error) => {
+  //           console.log(error.message);
+  //           setLoading(false);
+  //           setError(true);
+  //         });
+  //     }
+  //   }
+  // }, [user, location, search]);
+
+  // if (loading) {
+  //   return (
+  //     <CenteredInfo>
+  //       <h1>Proberen inloggen...</h1>
+  //     </CenteredInfo>
+  //   );
+  // }
+
+  // if (error) {
+  //   toast("Er is iets misgelopen bij het inloggen");
+  // }
+
+  if (!user) {
     return <Outlet />;
   }
 
