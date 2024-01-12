c92d03c98ddfba88253a9ea4f6e1ea0f826ecf28
Thibault <thibault.feraux@hotmail.com>
2024-01-12 17:36:49 +0100

    finished

--- app/src/components/app/auth/OnboardingLayout.jsx
+++ app/src/components/app/auth/OnboardingLayout.jsx
@@ -1,4 +1,4 @@
-import { isSignInWithEmailLink } from "firebase/auth";
+import { isSignInWithEmailLink, signInWithEmailLink } from "firebase/auth";
 import { useEffect, useState } from "react";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { auth } from "../../../core/services/firebase";
@@ -16,44 +16,43 @@ const OnboardingLayout = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
-  // useEffect(() => {
-  //   if (!user) {
-  //     if (isSignInWithEmailLink(auth, window.location.href)) {
-  //       let email = window.localStorage.getItem("emailForSignIn");
-  //       if (!email) {
-  //         // ask user for email
-  //         email = window.prompt("Geef uw e-mailadres voor bevestiging");
-  //       }
-  //       // sign in user
-  //       setLoading(true);
-  //       auth
-  //         .signInWithEmailLink(email, window.location.href)
-  //         .then(() => {
-  //           window.localStorage.removeItem("emailForSignIn");
-  //           console.log("signed in");
-  //           setLoading(false);
-  //           setError(false);
-  //         })
-  //         .catch((error) => {
-  //           console.log(error.message);
-  //           setLoading(false);
-  //           setError(true);
-  //         });
-  //     }
-  //   }
-  // }, [user, location, search]);
+  useEffect(() => {
+    if (!user) {
+      if (isSignInWithEmailLink(auth, window.location.href)) {
+        let email = window.localStorage.getItem("emailForSignIn");
+        if (!email) {
+          // ask user for email
+          email = window.prompt("Geef uw e-mailadres voor bevestiging");
+        }
+        // sign in user
+        setLoading(true);
+        signInWithEmailLink(auth, email, window.location.href)
+          .then(() => {
+            window.localStorage.removeItem("emailForSignIn");
+            console.log("signed in");
+            setLoading(false);
+            setError(false);
+          })
+          .catch((error) => {
+            console.log(error.message);
+            setLoading(false);
+            setError(true);
+          });
+      }
+    }
+  }, [user, location, search]);
 
-  // if (loading) {
-  //   return (
-  //     <CenteredInfo>
-  //       <h1>Proberen inloggen...</h1>
-  //     </CenteredInfo>
-  //   );
-  // }
+  if (loading) {
+    return (
+      <CenteredInfo>
+        <h1>Proberen inloggen...</h1>
+      </CenteredInfo>
+    );
+  }
 
-  // if (error) {
-  //   toast("Er is iets misgelopen bij het inloggen");
-  // }
+  if (error) {
+    toast("Er is iets misgelopen bij het inloggen");
+  }
 
   if (!user) {
     return <Outlet />;
