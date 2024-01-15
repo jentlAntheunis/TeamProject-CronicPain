28a72a73f387d954b02df412ac62f93f1ab82346
juul-vandevelde-howest <juul.van.de.velde@student.howest.be>
2024-01-15 13:31:23 +0100

    styling: positioned arrow left

--- app/src/components/ui/PageHeading/PageHeading.jsx
+++ app/src/components/ui/PageHeading/PageHeading.jsx
@@ -3,7 +3,7 @@ import { ArrowLeft } from '@phosphor-icons/react';
 
 const PageHeading = ({ children }) => (
   <div className={styles.pageHeading}>
-    <ArrowLeft size={32} />
+    <ArrowLeft size={32} className={styles.arrow}/>
     <h1>{children}</h1>
   </div>
 );
