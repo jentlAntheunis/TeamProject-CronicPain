227fbca0436ef67b7a62572b1565a55f2df86187
juul-vandevelde-howest <juul.van.de.velde@student.howest.be>
2024-01-15 14:14:51 +0100

    styling: centered the form

--- app/src/components/screens/Specialist/AddPatient/AddPatient.jsx
+++ app/src/components/screens/Specialist/AddPatient/AddPatient.jsx
@@ -1,4 +1,10 @@
-import { Form, FormControl, FormItem, FormLabel, FormMessage } from "../../../app/form/Form";
+import {
+  Form,
+  FormControl,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../../../app/form/Form";
 import Button from "../../../ui/Button/Button";
 import FullHeightScreen from "../../../ui/FullHeightScreen/FullHeightScreen";
 import Input from "../../../ui/Input/Input";
@@ -13,38 +19,39 @@ const formSchema = z.object({
 
 const AddPatient = () => {
   return (
-    <FullHeightScreen>
+    <FullHeightScreen className={styles.screen}>
       <PageHeading>Patiënt toevoegen</PageHeading>
-      <Form schema={formSchema}>
-      <FormItem name="last-name">
-        <FormLabel>Achternaam</FormLabel>
-        <FormControl>
-          <Input placeholder="Vul een achternaam in" />
-        </FormControl>
-        <FormMessage />
-      </FormItem>
-      <FormItem name="first-name">
-        <FormLabel>Voornaam</FormLabel>
-        <FormControl>
-          <Input placeholder="Vul een voornaam in" />
-        </FormControl>
-        <FormMessage />
-      </FormItem>
-      <FormItem name="email">
-        <FormLabel>E-mail</FormLabel>
-        <FormControl>
-          <Input placeholder="dirkjanssens@voorbeeld.be" autoComplete="email" />
-        </FormControl>
-        <FormMessage />
-      </FormItem>
-      <Button
-        type="submit"
-        size="full"
-        className={styles.submit}
-      >
-        Toevoegen
-      </Button>
-    </Form>
+      <Form schema={formSchema} className={styles.formContainer}>
+        <div className={styles.formItems}>
+          <FormItem name="last-name">
+            <FormLabel>Achternaam</FormLabel>
+            <FormControl>
+              <Input placeholder="Vul een achternaam in" />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+          <FormItem name="first-name">
+            <FormLabel>Voornaam</FormLabel>
+            <FormControl>
+              <Input placeholder="Vul een voornaam in" />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+          <FormItem name="email">
+            <FormLabel>E-mail</FormLabel>
+            <FormControl>
+              <Input
+                placeholder="dirkjanssens@voorbeeld.be"
+                autoComplete="email"
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        </div>
+        <Button type="submit" size="full" className={styles.submit}>
+          Toevoegen
+        </Button>
+      </Form>
     </FullHeightScreen>
   );
 };
