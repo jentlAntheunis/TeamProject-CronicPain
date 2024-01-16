bf98f7aa34276e8b6807d08b3ea4485f7abbffc0
juul-vandevelde-howest <juul.van.de.velde@student.howest.be>
2024-01-16 09:28:50 +0100

    feat: added logout popup

--- app/src/components/ui/TopBar/TopBar.jsx
+++ app/src/components/ui/TopBar/TopBar.jsx
@@ -1,11 +1,37 @@
+import { useState } from "react";
 import Coin from "../Icons/Coin";
 import Streaks from "../Icons/Streaks";
 import RewardMetric from "../RewardMetric/RewardMetric.jsx";
 
 import styles from "./TopBar.module.css";
-import { DotsThreeVertical } from "@phosphor-icons/react";
+import { DotsThreeVertical, SignOut, Scroll } from "@phosphor-icons/react";
+
+const Menu = ({ showMenu }) => {
+  if (!showMenu) {
+    return null;
+  }
+
+  return (
+    <div className={styles.menu}>
+      <button className={`btn-reset ${styles.menuItem}`}>
+        Uitloggen
+        <SignOut size={24} />
+      </button>
+      <button className={`btn-reset ${styles.menuItem}`}>
+        Credits
+        <Scroll size={24} />
+        </button>
+    </div>
+  );
+};
 
 const TopBar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const onClickDots = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <div className={styles.spaceBetween}>
       <div className={styles.flex}>
@@ -16,7 +42,15 @@ const TopBar = () => {
           <Streaks />
         </RewardMetric>
       </div>
-      <DotsThreeVertical size={32} weight="bold" className={styles.menu} />
+      <div className={styles.menuContainer}>
+        <DotsThreeVertical
+          size={32}
+          weight="bold"
+          className={styles.dots}
+          onClick={onClickDots}
+        />
+        <Menu showMenu={showMenu} />
+      </div>
     </div>
   );
 };
