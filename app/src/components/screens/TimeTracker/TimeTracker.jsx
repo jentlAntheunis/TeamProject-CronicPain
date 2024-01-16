13be1648d6014c020c75b16adcba0525cd1a3acf
juul-vandevelde-howest <juul.van.de.velde@student.howest.be>
2024-01-16 21:55:51 +0100

    feat: added timetracker

--- app/src/components/screens/TimeTracker/TimeTracker.jsx
+++ app/src/components/screens/TimeTracker/TimeTracker.jsx
@@ -1,5 +1,66 @@
+import { ArrowLeft, Play, Stop } from "@phosphor-icons/react";
+import Button from "../../ui/Button/Button";
+import FullHeightScreen from "../../ui/FullHeightScreen/FullHeightScreen";
+import styles from "./TimeTracker.module.css";
+import { useStopwatch } from "react-timer-hook";
+
 const TimeTracker = () => {
-  return (<div>Time</div>);
+  return (
+    <FullHeightScreen className={styles.screen}>
+      <button className={`btn-reset ${styles.backBtn}`}>
+        <ArrowLeft size={32} />
+      </button>
+      <MyStopwatch />
+    </FullHeightScreen>
+  );
+};
+
+function MyStopwatch() {
+  const {
+    totalSeconds,
+    seconds,
+    minutes,
+    hours,
+    isRunning,
+    start,
+    pause,
+    reset,
+  } = useStopwatch();
+
+  // https://www.npmjs.com/package/react-timer-hook
+
+  const formatTime = (value) => {
+    return value.toString().padStart(2, "0");
+  };
+
+  const handleButtonClick = () => {
+    if (isRunning) {
+      pause();
+    } else {
+      start();
+    }
+  };
+
+  return (
+    <>
+      <div className={styles.counter}>
+        <span>{formatTime(hours)}</span>:<span>{formatTime(minutes)}</span>:
+        <span>{formatTime(seconds)}</span>
+      </div>
+      <Button
+        size="full"
+        onClick={handleButtonClick}
+        className={isRunning ? styles.startStopBtn : ""}
+      >
+        {isRunning ? "Stop met bewegen" : "Start met bewegen"}
+        {isRunning ? (
+          <Stop size={22} weight="bold" />
+        ) : (
+          <Play size={22} weight="bold" />
+        )}
+      </Button>
+    </>
+  );
 }
 
 export default TimeTracker;
