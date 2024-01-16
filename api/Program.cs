d3b2052a693f32d5154c391df2bde8cdd5e52111
liendejong <liendejong03@gmail.com>
2024-01-16 16:14:43 +0100

    Added authorization to swagger ui

--- api/Program.cs
+++ api/Program.cs
@@ -27,29 +27,33 @@ builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
+builder.Services.AddSwaggerGen(opt =>
 {
     // Define the FirebaseAuthentication security scheme
-    var firebaseSecurityScheme = new OpenApiSecurityScheme
+    opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
-        Type = SecuritySchemeType.ApiKey,
-        Name = "Authorization",
         In = ParameterLocation.Header,
-        Description = "Bearer {your_access_token}",
-        Scheme = "Bearer"
-    };
+        Description = "Please enter token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "bearer"
+    });
 
-    // Define the FirebaseAuthentication security requirement
-    var firebaseSecurityRequirement = new OpenApiSecurityRequirement
+    opt.AddSecurityRequirement(new OpenApiSecurityRequirement
     {
         {
-            firebaseSecurityScheme, new string[] {}
-        },
-    };
-
-    // Add the security definition and requirement to Swagger
-    c.AddSecurityDefinition("FirebaseAuthentication", firebaseSecurityScheme);
-    c.AddSecurityRequirement(firebaseSecurityRequirement);
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
 });
 
 
