8d124d0cc9c0f500de7bf346e609949a7cb1ed79
liendejong <liendejong03@gmail.com>
2024-01-16 09:40:13 +0100

    Changed middleware: added user information to the context

--- api/Middleware/FirebaseTokenValidatorMiddleware.cs
+++ api/Middleware/FirebaseTokenValidatorMiddleware.cs
@@ -4,6 +4,7 @@ public class FirebaseTokenValidatorMiddleware
 {
     private readonly RequestDelegate _next;
 
+    // Allows the request to pass to the next middleware in the pipeline if the token is valid
     public FirebaseTokenValidatorMiddleware(RequestDelegate next)
     {
         _next = next;
@@ -11,18 +12,23 @@ public class FirebaseTokenValidatorMiddleware
 
     public async Task InvokeAsync(HttpContext context)
     {
+        // Get the token from the Authorization header
         string token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
         if (!string.IsNullOrEmpty(token))
         {
             try
             {
+                // Verify the token
                 var decodedToken = await FirebaseAuth.DefaultInstance.VerifyIdTokenAsync(token);
-                // Add user information to the context or perform additional checks
+                Console.WriteLine("Token is valid");
+                // Add user information to the context
+                context.Items["UserEmail"] = decodedToken.Claims["email"].ToString();
             }
             catch (FirebaseAuthException)
             {
-                // Token is invalid - handle accordingly
+                // Token is invalid
                 context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                Console.WriteLine("Token is invalid");
                 return;
             }
         }
