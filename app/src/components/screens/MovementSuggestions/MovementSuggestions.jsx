0e3d1cf190670742a9f67ab0bb0c6832eed5ba52
juul-vandevelde-howest <juul.van.de.velde@student.howest.be>
2024-01-16 19:07:11 +0100

    feat: added movement suggestions screen

--- app/src/components/screens/MovementSuggestions/MovementSuggestions.jsx
+++ app/src/components/screens/MovementSuggestions/MovementSuggestions.jsx
@@ -1,5 +1,29 @@
+import Button from "../../ui/Button/Button";
+import FullHeightScreen from "../../ui/FullHeightScreen/FullHeightScreen";
+import PageHeading from "../../ui/PageHeading/PageHeading";
+import styles from "./MovementSuggestions.module.css";
+
 const MovementSuggestions = () => {
-    return (<div>Move</div>);
-}
+  return (
+    <FullHeightScreen className={styles.screen}>
+      <div className={styles.layout}>
+      <PageHeading>Kies een activiteit</PageHeading>
+        <div className={styles.suggestions}>
+          <button className={`btn-reset ${styles.suggestion}`}>
+            Spieroefeningen
+          </button>
+          <button className={`btn-reset ${styles.suggestion}`}>Wandelen</button>
+          <button className={`btn-reset ${styles.suggestion}`}>
+            Huishoudelijke taken
+          </button>
+          <button className={`btn-reset ${styles.suggestion}`}>
+            Vrij bewegen
+          </button>
+        </div>
+      <Button size="full">Volgende</Button>
+      </div>
+    </FullHeightScreen>
+  );
+};
 
 export default MovementSuggestions;
