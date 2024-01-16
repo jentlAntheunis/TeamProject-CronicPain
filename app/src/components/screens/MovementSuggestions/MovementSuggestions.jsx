3e366fde2cce9cb947dd1b105fe21dec264dbcae
juul-vandevelde-howest <juul.van.de.velde@student.howest.be>
2024-01-16 19:18:29 +0100

    feat: added functionalillity to select an option

--- app/src/components/screens/MovementSuggestions/MovementSuggestions.jsx
+++ app/src/components/screens/MovementSuggestions/MovementSuggestions.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from "react";
 import Button from "../../ui/Button/Button";
 import FullHeightScreen from "../../ui/FullHeightScreen/FullHeightScreen";
 import PageHeading from "../../ui/PageHeading/PageHeading";
@@ -12,18 +13,49 @@ const MovementSuggestions = () => {
     // Add more suggestions here
   ];
 
+  const [selectedSuggestion, setSelectedSuggestion] = useState(null);
+  const suggestionsContainerRef = useRef(null);
+
+  const handleSuggestionClick = (index) => {
+    setSelectedSuggestion(index);
+  };
+
+  const handleClickOutsideSuggestions = (event) => {
+    if (
+      suggestionsContainerRef.current &&
+      !suggestionsContainerRef.current.contains(event.target)
+    ) {
+      setSelectedSuggestion(null);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutsideSuggestions);
+    return () => {
+      document.removeEventListener("click", handleClickOutsideSuggestions);
+    };
+  }, []);
+
   return (
     <FullHeightScreen className={styles.screen}>
       <div className={styles.layout}>
         <PageHeading>Kies een activiteit</PageHeading>
-        <div className={styles.suggestions}>
+        <div className={styles.suggestions} ref={suggestionsContainerRef}>
           {suggestions.map((suggestion, index) => (
-            <button key={index} className={`btn-reset ${styles.suggestion}`}>
+            <button
+              key={index}
+              className={`btn-reset ${styles.suggestion} ${
+                selectedSuggestion === index ? styles.selected : ""
+              }`}
+              onClick={() => handleSuggestionClick(index)}
+            >
               {suggestion}
             </button>
           ))}
         </div>
-        <Button size="full">Volgende</Button>
+        <Button size="full" disabled={selectedSuggestion === null}>
+          Volgende
+        </Button>
       </div>
     </FullHeightScreen>
   );
