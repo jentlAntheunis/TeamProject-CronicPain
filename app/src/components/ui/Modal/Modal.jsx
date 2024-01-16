3fc92492fd5f49dbf2d2019d4f13554656c16d7b
juul-vandevelde-howest <juul.van.de.velde@student.howest.be>
2024-01-16 11:30:28 +0100

    feat: added Modal

---
+++ app/src/components/ui/Modal/Modal.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import styles from "./Modal.module.css";
+
+const Modal = ({ showModal, setShowModal, children }) => {
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  if (!showModal) {
+    return null;
+  }
+
+  return (
+    <div className={styles.modal}>
+      <div className={styles.modalBackground} onClick={handleCloseModal}></div>
+      <div className={styles.modalContent}>{children}</div>
+    </div>
+  );
+};
+
+export default Modal;
--- app/src/components/ui/TopBar/TopBar.jsx
+++ app/src/components/ui/TopBar/TopBar.jsx
@@ -5,31 +5,21 @@ import RewardMetric from "../RewardMetric/RewardMetric.jsx";
 
 import styles from "./TopBar.module.css";
 import { DotsThreeVertical, SignOut, Scroll } from "@phosphor-icons/react";
-
-const Menu = ({ showMenu }) => {
-  if (!showMenu) {
-    return null;
-  }
-
-  return (
-    <div className={styles.menu}>
-      <button className={`btn-reset ${styles.menuItem}`}>
-        Uitloggen
-        <SignOut size={24} />
-      </button>
-      <button className={`btn-reset ${styles.menuItem}`}>
-        Credits
-        <Scroll size={24} />
-        </button>
-    </div>
-  );
-};
+import Modal from "../Modal/Modal.jsx";
+import Button from "../Button/Button.jsx";
+import { auth } from "../../../core/services/firebase.js";
 
 const TopBar = () => {
   const [showMenu, setShowMenu] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
   const onClickDots = () => {
     setShowMenu(!showMenu);
+  };
+
+  const handleLogout = () => {
+    setShowModal(false);
+    auth.signOut();
   };
 
   return (
@@ -49,8 +39,52 @@ const TopBar = () => {
           className={styles.dots}
           onClick={onClickDots}
         />
-        <Menu showMenu={showMenu} />
+        <Menu showMenu={showMenu} setShowMenu={setShowMenu} setShowModal={setShowModal} />
       </div>
+      <Modal showModal={showModal} setShowModal={setShowModal}>
+          <div>
+            <div className={styles.modalText}>
+              Weet je zeker dat je wilt uitloggen?
+            </div>
+            <div className={styles.modalButtons}>
+              <Button
+                variant="tertiary"
+                size="small"
+                className="btn-reset"
+                onClick={() => setShowModal(false)}
+              >
+                Annuleren
+              </Button>
+              <Button
+                size="small"
+                className="btn-reset"
+                onClick={handleLogout}
+              >
+                Uitloggen
+              </Button>
+            </div>
+          </div>
+        </Modal>
+    </div>
+  );
+};
+
+const Menu = ({ showMenu, setShowMenu, setShowModal }) => {
+
+  if (!showMenu) {
+    return null;
+  }
+
+  return (
+    <div className={styles.menu}>
+      <button className={`btn-reset ${styles.menuItem}`} onClick={() => { setShowModal(true); setShowMenu(false); }}>
+        Uitloggen
+        <SignOut size={24} />
+      </button>
+      <button className={`btn-reset ${styles.menuItem}`}>
+        Credits
+        <Scroll size={24} />
+      </button>
     </div>
   );
 };
