ce53353791579a45291ba0a27d17969ebbf1bdb0
juul-vandevelde-howest <juul.van.de.velde@student.howest.be>
2024-01-22 14:36:24 +0100

    feat: added filled in questionnaires

--- app/src/components/screens/Specialist/PatientDetails/PatientDetails.jsx
+++ app/src/components/screens/Specialist/PatientDetails/PatientDetails.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import ScrollableScreen from "../../../ui/ScrollableScreen/ScrollableScreen";
 import NavBar from "../../../ui/NavBar/NavBar";
 import PageHeading from "../../../ui/PageHeading/PageHeading";
@@ -6,10 +6,59 @@ import { SpecialistRoutes } from "../../../../core/config/routes";
 import styles from "./PatientDetails.module.css";
 import MovingInfluenceCard from "../../../ui/MovingInfluenceCard/MovingInfluanceCard";
 import Graph from "../../../ui/Graph/Graph";
+import Button from "../../../ui/Button/Button";
 
 const PatientDetails = () => {
   let { id } = useParams();
-  console.log(id);
+
+  const questionnaires = [
+    {
+      category: "bewegingsvragen",
+      datetime: "03/01/2024 14:03",
+    },
+    {
+      category: "bonusvragen",
+      datetime: "04/01/2024 15:12",
+    },
+    {
+      category: "bewegingsvragen",
+      datetime: "04/01/2024 10:45",
+    },
+    {
+      category: "bewegingsvragen",
+      datetime: "04/01/2024 09:27",
+    },
+    {
+      category: "bonusvragen",
+      datetime: "05/01/2024 16:58",
+    },
+    {
+      category: "bewegingsvragen",
+      datetime: "05/01/2024 11:30",
+    },
+    {
+      category: "bewegingsvragen",
+      datetime: "05/01/2024 13:15",
+    },
+    {
+      category: "bewegingsvragen",
+      datetime: "05/01/2024 08:59",
+    },
+    {
+      category: "bonusvragen",
+      datetime: "05/01/2024 17:42",
+    },
+    {
+      category: "bonusvragen",
+      datetime: "06/01/2024 12:20",
+    },
+  ];
+
+  const sortedQuestionnaires = [...questionnaires].sort((a, b) => {
+    const dateA = new Date(a.datetime);
+    const dateB = new Date(b.datetime);
+    return dateA - dateB;
+  });
 
   return (
     <ScrollableScreen>
@@ -27,16 +76,39 @@ const PatientDetails = () => {
           </div>
         </div>
         <div className={styles.graphs}>
-            <Graph variant={"bar"} title={"Overzicht tijdsduur beweging voorbije week"}></Graph>
-            <Graph variant={"line"} title={"Pijn ervaring voorbije maand"}></Graph>
+          <Graph
+            variant={"bar"}
+            title={"Overzicht tijdsduur beweging voorbije week"}
+          ></Graph>
+          <Graph
+            variant={"line"}
+            title={"Pijn ervaring voorbije maand"}
+          ></Graph>
         </div>
         <div className={styles.questionnairesContainer}>
-          <div>
+          <div className={styles.titleAndSearchContainer}>
             <div className={styles.h5}>Vragenlijsten</div>
-            <div>search</div>
-            <div className={styles.questionnaires}>
-              <div className={styles.questionnaire}></div>
-            </div>
+            <input
+              type="date"
+              name="date"
+              id="date"
+              className={styles.searchOnDate}
+            />
+          </div>
+          <div className={styles.questionnaires}>
+            {sortedQuestionnaires.map((questionnaire, index) => (
+              <div key={index} className={styles.questionnaire}>
+                <div>
+                  <div className={styles.datetime}>{questionnaire.datetime}</div>
+                  <div className={styles.category}>{questionnaire.category}</div>
+                </div>
+                <Link to={"/nogintevullen"}>
+                  <Button variant="tertiary" size="superSmall">
+                    Details
+                  </Button>
+                </Link>
+              </div>
+            ))}
           </div>
         </div>
       </div>
