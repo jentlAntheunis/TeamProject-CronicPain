cd84e34f0d6130ee9e35b975b0d56960ba753dc8
jentlAntheunis <113892951+jentlAntheunis@users.noreply.github.com>
2024-01-22 13:32:57 +0100

    Get specialist patient list done

--- api/Repositories/PatientRepository.cs
+++ api/Repositories/PatientRepository.cs
@@ -8,6 +8,7 @@ public interface IPatientRepository
 {
     Task<List<Patient>> GetAllPatientsAsync();
     Task<Patient> GetPatientByIdAsync(Guid id);
+    Task<List<Patient>> GetPatientsBySpecialistIdAsync(Guid id);
     Task<Patient> GetPatientDetailsByIdAsync(Guid id);
     Task<Guid> CreatePatientAsync(Patient patient);
     Task<Patient> UpdatePatientAsync(Patient patient);
@@ -26,6 +27,7 @@ public class PatientRepository : IPatientRepository
     public async Task<List<Patient>> GetAllPatientsAsync()
     {
         return await _context.Patient
+            .Include(p => p.Avatar)
             .Where(p => p.IsDeleted == false)
             .ToListAsync();
     }
@@ -41,13 +43,21 @@ public class PatientRepository : IPatientRepository
     public async Task<Patient> GetPatientDetailsByIdAsync(Guid id)
     {
         return await _context.Patient
-        .Include(p => p.Avatar)
-            .ThenInclude(a => a.Color)
-        .Include(p => p.Colors)
-        .Include(p => p.MovementSessions)
-        .Include(p => p.MovementSuggestions)
-        .Include(p => p.Logins)
-        .FirstOrDefaultAsync(p => p.Id == id);
+            .Include(p => p.Avatar).ThenInclude(a => a.Color)
+            .Include(p => p.Colors)
+            .Include(p => p.MovementSessions)
+            .Include(p => p.MovementSuggestions)
+            .Include(p => p.Logins)
+            .FirstOrDefaultAsync(p => p.Id == id);
+    }
+
+    public async Task<List<Patient>> GetPatientsBySpecialistIdAsync(Guid id)
+    {
+        return await _context.Patient
+            .Include(p => p.Avatar)
+            .Include(p => p.Specialists)
+            .Where(p => p.Specialists.Any(s => s.Id == id))
+            .ToListAsync();
     }
 
     public async Task<Guid> CreatePatientAsync(Patient patient)
