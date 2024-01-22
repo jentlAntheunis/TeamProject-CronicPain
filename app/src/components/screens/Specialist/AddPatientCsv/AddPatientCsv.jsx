67009e0a61348cb6aeda3b27e962dc40575b9437
Thibault <thibault.feraux@hotmail.com>
2024-01-22 21:25:41 +0100

    feat: validate cvs input and transform to object

--- app/src/components/screens/Specialist/AddPatientCsv/AddPatientCsv.jsx
+++ app/src/components/screens/Specialist/AddPatientCsv/AddPatientCsv.jsx
@@ -13,22 +13,70 @@ import PageHeading from "../../../ui/PageHeading/PageHeading";
 import ScrollableScreen from "../../../ui/ScrollableScreen/ScrollableScreen";
 import styles from "./AddPatientCsv.module.css";
 import { z } from "zod";
+import Papa from "papaparse";
+import { validatePatientCsv } from "../../../../core/utils/csv";
 
 const formSchema = z.object({
   csv: z
-    .string({ message: "Upload een CSV bestand" })
-    .min(1, { message: "Upload een CSV bestand" }),
+    .any()
+    .refine((data) => {
+      // check if any file is uploaded
+      if (data.length === 0) {
+        return false;
+      }
+      return true;
+    }, "Geen bestand geüpload")
+    .refine((data) => {
+      // check if file is csv
+      const file = data[0];
+      if (!file) {
+        return false;
+      }
+      const allowedExtensions = [".csv"];
+      const fileExtension = file.name.substring(file.name.lastIndexOf("."));
+      return allowedExtensions.includes(fileExtension);
+    }, "Geen geldig bestandstype"),
 });
 
 const AddPatientCsv = () => {
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const defaultValues = {
     csv: "",
   };
 
-  const handleSubmit = (data) => {
-    console.log(data);
+  const handleSubmit = ({ csv }) => {
+    setLoading(true);
+    setError(null);
+    const file = csv[0];
+    Papa.parse(file, {
+      complete: function (results) {
+        console.log(results);
+        const data = results.data;
+
+        try {
+          validatePatientCsv(data);
+        } catch (error) {
+          setError(error.message);
+          setLoading(false);
+          return;
+        }
+
+        // transform data to array of objects
+        const patients = data.map((patient) => {
+          return {
+            lastName: patient[0],
+            firstName: patient[1],
+            email: patient[2],
+          };
+        });
+
+        // TODO: send to backend
+        console.log(patients);
+        setLoading(false);
+      },
+    });
   };
 
   return (
@@ -39,8 +87,10 @@ const AddPatientCsv = () => {
           Patiënten toevoegen op basis van CSV
         </PageHeading>
         <p className={styles.infoText}>
-          Upload een csv bestand met enkel de familienaam, voornaam en
-          e-mailadres van de patiënten
+          Upload een csv bestand met enkel de{" "}
+          <strong>familienaam, voornaam en e-mailadres</strong> van de patiënten
+          in deze volgorde. Het bestand mag <strong>geen kolomnamen</strong>{" "}
+          bevatten.
         </p>
         <Form
           schema={formSchema}
@@ -51,7 +101,7 @@ const AddPatientCsv = () => {
             <FormControl>
               <FileInput />
             </FormControl>
-            <FormMessage />
+            <FormMessage>{error}</FormMessage>
           </FormItem>
           <div className={`mobile-only ${styles.removePadding}`}>
             <Button type="submit" size="full" disabled={loading}>
---
+++ app/src/core/utils/csv.js
@@ -0,0 +1,60 @@
+import { z } from "zod";
+
+const validatePatientCsv = (data) => {
+  const hasThreeColumns = data.every((row) => row.length === 3);
+
+  if (!hasThreeColumns) {
+    throw new Error("Het bestand bevat niet 3 kolommen");
+  }
+
+  const possibleColumnNames = [
+    "familienaam",
+    "achternaam",
+    "lastname",
+    "voornaam",
+    "firstname",
+    "email",
+    "e-mail",
+    "e-mailadres",
+    "emailadres",
+  ];
+
+  const hasNoColumnNames = data.every((row) => {
+    return row.every(
+      (field) => !possibleColumnNames.includes(field.toLowerCase())
+    );
+  });
+
+  if (!hasNoColumnNames) {
+    throw new Error("Het bestand bevat kolomnamen");
+  }
+
+  const hasValidEmails = data.every((row) => {
+    const email = row[2];
+    return z.string().email().safeParse(email).success;
+  });
+
+  if (!hasValidEmails) {
+    throw new Error("Het bestand bevat ongeldige e-mailadressen");
+  }
+
+  const hasNoEmptyFields = data.every((row) => {
+    return row.every((field) => field !== "");
+  });
+
+  if (!hasNoEmptyFields) {
+    throw new Error("Het bestand bevat lege velden");
+  }
+
+  const hasNoDuplicates = data.every((row, index) => {
+    const email = row[2];
+    const emails = data.map((row) => row[2]);
+    return emails.indexOf(email) === index;
+  });
+
+  if (!hasNoDuplicates) {
+    throw new Error("Het bestand bevat dubbele e-mailadressen");
+  }
+}
+
+export { validatePatientCsv }
