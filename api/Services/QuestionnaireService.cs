dee437012b8d67e5d89e69ab4e75b2f921b15df5
Thibault <thibault.feraux@hotmail.com>
2024-01-25 11:03:42 +0100

    merge: manual merge from feature/fix-get-only-filled-in-questionnaires

--- api/Services/QuestionnaireService.cs
+++ api/Services/QuestionnaireService.cs
@@ -13,129 +13,176 @@ using Microsoft.EntityFrameworkCore;
 
 public interface IQuestionnaireService
 {
-    Task<Questionnaire> GetQuestionnaireByIdAsync(Guid id);
-    Task<List<Questionnaire>> GetQuestionnairesByPatientIdAsync(Guid id);
-    Task<QuestionnaireDTO> AddMovementQuestionnaireAsync(Guid id);
-    Task<QuestionnaireDTO> AddBonusQuestionnaireAsync(Guid userId);
+  Task<Questionnaire> GetQuestionnaireByIdAsync(Guid id);
+  Task<List<Questionnaire>> GetQuestionnairesByPatientIdAsync(Guid id);
+  // Task<QuestionnaireDTO> AddMovementQuestionnaireAsync(Guid id);
+  // Task<QuestionnaireDTO> AddBonusQuestionnaireAsync(Guid userId);
+  // Task<QuestionnaireDTO> AddDailyPainQuestionnaireAsync(Guid userId);
+  // Task<Questionnaire> UpdateQuestionnaireAsync(Questionnaire questionnaire);
+  // Task UpdateQuestionnaireIndexAsync(Guid questionnaireId, int questionnaireIndex);
+  // Task DeleteQuestionnaireAsync(Questionnaire questionnaire);
+  // Task<List<Questionnaire>> GetQuestionnairesAsync();
 
-    Task<QuestionnaireDTO> AddDailyPainQuestionnaireAsync(Guid userId);
-    Task<Questionnaire> UpdateQuestionnaireAsync(Questionnaire questionnaire);
-    Task UpdateQuestionnaireIndexAsync(Guid questionnaireId, int questionnaireIndex);
-    Task DeleteQuestionnaireAsync(Questionnaire questionnaire);
-    Task<List<Questionnaire>> GetQuestionnairesAsync();
+  Task<bool> CheckIfFirstQuestionnaireOfTheDay(Guid userId);
+  Task<bool> CheckIfFirstBonusOfTheDay(Guid userId);
 
-    Task<bool> CheckIfFirstQuestionnaireOfTheDay(Guid userId);
-
-    Task<List<QuestionnaireDetailDTO>> GetQuestionnairesWithDetailsByPatientIdAsync(Guid patientId, List<string> categories);
+  Task<List<QuestionnaireDetailDTO>> GetQuestionnairesWithDetailsByPatientIdAsync(Guid patientId, List<string> categories);
 
 
 }
 
 public class QuestionnaireService : IQuestionnaireService
 {
-    private readonly IQuestionnaireRepository _questionnaireRepository;
-    private readonly IQuestionRepository _questionRepository;
-    private readonly IOptionRepository _optionRepository;
-    private readonly IAnswerRepository _answerRepository;
-    private readonly IAnswerService _answerService;
+  private readonly IQuestionnaireRepository _questionnaireRepository;
+  private readonly IQuestionRepository _questionRepository;
+  private readonly IOptionRepository _optionRepository;
+  private readonly IAnswerRepository _answerRepository;
+  private readonly IAnswerService _answerService;
 
-    private readonly PebblesContext _context;
+  private readonly PebblesContext _context;
 
-    public QuestionnaireService(
-        IQuestionnaireRepository questionnaireRepository,
-        IQuestionRepository questionRepository,
-        IOptionRepository optionRepository,
-        IAnswerRepository answerRepository,
-        IAnswerService answerService,
-        PebblesContext context
-        )
+  public QuestionnaireService(
+      IQuestionnaireRepository questionnaireRepository,
+      IQuestionRepository questionRepository,
+      IOptionRepository optionRepository,
+      IAnswerRepository answerRepository,
+      IAnswerService answerService,
+      PebblesContext context
+      )
+  {
+    _questionnaireRepository = questionnaireRepository;
+    _questionRepository = questionRepository;
+    _optionRepository = optionRepository;
+    _answerRepository = answerRepository;
+    _answerService = answerService;
+    _context = context;
+  }
+
+  public async Task<Questionnaire> GetQuestionnaireByIdAsync(Guid id) => await _questionnaireRepository.GetQuestionnaireByIdAsync(id);
+
+  public async Task<List<Questionnaire>> GetQuestionnairesByPatientIdAsync(Guid id) => await _questionnaireRepository.GetQuestionnairesByPatientIdAsync(id);
+
+  // public async Task<QuestionnaireDTO> AddMovementQuestionnaireAsync(Guid id) => throw new NotImplementedException();
+  // public async Task<QuestionnaireDTO> AddBonusQuestionnaireAsync(Guid userId) => throw new NotImplementedException();
+  // public async Task<QuestionnaireDTO> AddDailyPainQuestionnaireAsync(Guid userId) => throw new NotImplementedException();
+  // public async Task<Questionnaire> UpdateQuestionnaireAsync(Questionnaire questionnaire) => throw new NotImplementedException();
+  // public async Task UpdateQuestionnaireIndexAsync(Guid questionnaireId, int questionnaireIndex) => throw new NotImplementedException();
+  // public async Task DeleteQuestionnaireAsync(Questionnaire questionnaire) => throw new NotImplementedException();
+  // public async Task<List<Questionnaire>> GetQuestionnairesAsync() => throw new NotImplementedException();
+
+
+  public async Task<bool> CheckIfFirstQuestionnaireOfTheDay(Guid userId)
+  {
+    DateTime currentDate = DateTime.Now.Date;
+
+    var questionnaireExists = await _context.Questionnaire
+        .AnyAsync(q => q.PatientId == userId && q.Date.HasValue && q.Date.Value.Date == currentDate);
+
+    bool isFirstQuestionnaire = !questionnaireExists;
+    if (isFirstQuestionnaire)
     {
-        _questionnaireRepository = questionnaireRepository;
-        _questionRepository = questionRepository;
-        _optionRepository = optionRepository;
-        _answerRepository = answerRepository;
-        _answerService = answerService;
-        _context = context;
+      var patient = await _context.Patient
+          .FirstOrDefaultAsync(p => p.Id == userId);
+
+      patient.Streak += 1;
+      await _context.SaveChangesAsync();
     }
+    return isFirstQuestionnaire;
+  }
 
-    public async Task<Questionnaire> GetQuestionnaireByIdAsync(Guid id) => await _questionnaireRepository.GetQuestionnaireByIdAsync(id);
+  public async Task<bool> CheckIfFirstBonusOfTheDay(Guid userId)
+  {
+    DateTime currentDate = DateTime.Now.Date;
 
-    public async Task<List<Questionnaire>> GetQuestionnairesByPatientIdAsync(Guid id) => await _questionnaireRepository.GetQuestionnairesByPatientIdAsync(id);
+    var questionnaireExists = await _context.Questionnaire
+        .Include(q => q.Questions)
+            .ThenInclude(q => q.Category)
+        .Where(q => q.Questions.Any(q => q.Category.Name == "bonus"))
+        .AnyAsync(q => q.PatientId == userId && q.Date.HasValue && q.Date.Value.Date == currentDate);
 
-    public async Task<QuestionnaireDTO> AddMovementQuestionnaireAsync(Guid id) => throw new NotImplementedException();
-
-    public async Task<QuestionnaireDTO> AddBonusQuestionnaireAsync(Guid userId) => throw new NotImplementedException();
-
-    public async Task<QuestionnaireDTO> AddDailyPainQuestionnaireAsync(Guid userId) => throw new NotImplementedException();
-    public async Task<Questionnaire> UpdateQuestionnaireAsync(Questionnaire questionnaire) => throw new NotImplementedException();
-    public async Task UpdateQuestionnaireIndexAsync(Guid questionnaireId, int questionnaireIndex) => throw new NotImplementedException();
-    public async Task DeleteQuestionnaireAsync(Questionnaire questionnaire) => throw new NotImplementedException();
-    public async Task<List<Questionnaire>> GetQuestionnairesAsync() => throw new NotImplementedException();
-
-
-    public async Task<bool> CheckIfFirstQuestionnaireOfTheDay(Guid userId)
+    bool isFirstQuestionnaire = !questionnaireExists;
+    if (isFirstQuestionnaire)
     {
-        DateTime currentDate = DateTime.Now.Date;
+      var patient = await _context.Patient
+          .FirstOrDefaultAsync(p => p.Id == userId);
 
-        var questionnaireExists = await _context.Questionnaire
-            .AnyAsync(q => q.PatientId == userId && q.Date.HasValue && q.Date.Value.Date == currentDate);
+      patient.Streak += 1;
+      await _context.SaveChangesAsync();
+    }
+    return isFirstQuestionnaire;
+  }
 
-        bool isFirstQuestionnaire = !questionnaireExists;
-        if (isFirstQuestionnaire)
+  public async Task<List<QuestionnaireDetailDTO>> GetQuestionnairesWithDetailsByPatientIdAsync(Guid patientId, List<string> categories)
+  {
+    try
+    {
+      var questionnaires = await _questionnaireRepository.GetFullQuestionnairesByPatientIdAsync(patientId);
+
+      if (questionnaires == null || !questionnaires.Any())
+      {
+        Console.WriteLine($"No questionnaires found for patientId: {patientId}");
+        return new List<QuestionnaireDetailDTO>();
+      }
+
+      var detailedQuestionnaires = new List<QuestionnaireDetailDTO>();
+
+      foreach (var questionnaire in questionnaires.Where(q => q.Date != null))
+      {
+        var firstQuestionCategory = questionnaire.Questions.FirstOrDefault()?.Category;
+        var questionnaireCategory = await _context.Category.FirstOrDefaultAsync(c => c.Id == firstQuestionCategory.Id);
+
+        var detailedQuestions = new List<QuestionDetailDTO>();
+
+        foreach (var question in questionnaire.Questions)
         {
-            var patient = await _context.Patient
-                .FirstOrDefaultAsync(p => p.Id == userId);
+          var questionCategory = await _context.Category.FirstOrDefaultAsync(c => c.Id == question.CategoryId);
+          if (questionCategory != null && categories.Contains(questionCategory.Name))
+          {
+            var filteredAnswers = question.Answers
+                .Select(a =>
+                {
+                  var option = _context.Option.FirstOrDefault(o => o.Id == a.OptionId);
+                  return new AnswerDTO
+                  {
+                    QuestionId = a.QuestionId,
+                    OptionId = a.OptionId,
+                    OptionContent = option?.Content,
+                    QuestionnaireIndex = a.QuestionnaireIndex
+                  };
+                }).ToList();
 
-            patient.Streak += 1;
-            await _context.SaveChangesAsync();
+            detailedQuestions.Add(new QuestionDetailDTO
+            {
+              Id = question.Id,
+              Content = question.Content,
+              Answers = filteredAnswers
+            });
+          }
         }
-        return isFirstQuestionnaire;
-    }
 
-    public async Task<List<QuestionnaireDetailDTO>> GetQuestionnairesWithDetailsByPatientIdAsync(Guid patientId, List<string> categories)
-    {
-        var questionnaires = await _questionnaireRepository.GetFullQuestionnairesByPatientIdAsync(patientId);
-        var detailedQuestionnaires = new List<QuestionnaireDetailDTO>();
-
-        foreach (var questionnaire in questionnaires)
+        if (detailedQuestions.Any())
         {
-            var detailedQuestions = new List<QuestionDetailDTO>();
-
-            foreach (var question in questionnaire.Questions)
-            {
-                // Check if the question's category is "beweging" or "bonus"
-                if (categories.Contains(question.Category.Name))
-                {
-                    var filteredAnswers = question.Answers
-                        .Where(a => a.QuestionnaireId == questionnaire.Id && (a.QuestionnaireIndex == 0 || a.QuestionnaireIndex == 1))
-                        .Select(a => new AnswerDTO
-                        {
-                            QuestionId = a.QuestionId,
-                            OptionId = a.OptionId,
-                            QuestionnaireIndex = a.QuestionnaireIndex
-                        }).ToList();
-
-                    detailedQuestions.Add(new QuestionDetailDTO
-                    {
-                        Id = question.Id,
-                        Content = question.Content,
-                        Answers = filteredAnswers
-                    });
-                }
-            }
-
-            // Add the questionnaire to the list only if it contains relevant questions
-            if (detailedQuestions.Any())
-            {
-                detailedQuestionnaires.Add(new QuestionnaireDetailDTO
-                {
-                    Id = questionnaire.Id,
-                    Date = questionnaire.Date,
-                    Questions = detailedQuestions
-                });
-            }
+          detailedQuestionnaires.Add(new QuestionnaireDetailDTO
+          {
+            Id = questionnaire.Id,
+            Date = questionnaire.Date,
+            PatientId = questionnaire.PatientId,
+            CategoryName = questionnaireCategory?.Name,
+            Questions = detailedQuestions
+          });
         }
+      }
 
-        return detailedQuestionnaires;
+      return detailedQuestionnaires;
     }
+    catch (Exception ex)
+    {
+      Console.WriteLine($"Error occurred: {ex.Message}");
+      throw;
+    }
+  }
+
+
+
+
 }
