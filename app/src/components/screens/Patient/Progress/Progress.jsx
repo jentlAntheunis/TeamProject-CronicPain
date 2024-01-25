d65f33f8d1a8f0aae0f163326dc269576b553acf
Thibault <thibault.feraux@hotmail.com>
2024-01-25 13:33:40 +0100

    feat: data on progress screen

--- app/src/components/screens/Patient/Progress/Progress.jsx
+++ app/src/components/screens/Patient/Progress/Progress.jsx
@@ -1,5 +1,10 @@
 import { toast } from "react-toastify";
-import { getUserData } from "../../../../core/utils/apiCalls";
+import {
+  getImpact,
+  getMovementWeek,
+  getPainMonth,
+  getUserData,
+} from "../../../../core/utils/apiCalls";
 import { useQuery } from "@tanstack/react-query";
 import { useUser } from "../../../app/auth/AuthProvider";
 import ScrollableScreen from "../../../ui/ScrollableScreen/ScrollableScreen";
@@ -13,7 +18,9 @@ import clsx from "clsx";
 import Modal from "../../../ui/Modal/Modal";
 import { X } from "@phosphor-icons/react";
 import Button from "../../../ui/Button/Button";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Impacts } from "../../../../core/config/impacts";
+import { fillMissingDates } from "../../../../core/utils/patientDetails";
 
 const Progress = () => {
   const [showModal, setShowModal] = useState(false);
@@ -25,6 +32,23 @@ const Progress = () => {
     queryKey: ["user"],
     queryFn: () => getUserData(user.id),
   });
+  const { data: impactData } = useQuery({
+    queryKey: ["impact", user.id],
+    queryFn: () => getImpact(user.id),
+  });
+  const { data: movementData } = useQuery({
+    queryKey: ["movement", user.id],
+    queryFn: () => getMovementWeek(user.id),
+  });
+  const { data: painData } = useQuery({
+    queryKey: ["pain", user.id],
+    queryFn: () => getPainMonth(user.id),
+  });
+
+  useEffect(() => {
+    console.log(painData)
+    console.log(user.id)
+  }, [painData])
 
   if (isLoading) return null;
 
@@ -67,24 +91,44 @@ const Progress = () => {
         />
       </div>
       <div className={styles.movingInfluenceCardsContainer}>
-        <MovingInfluenceCard variant={"positive"} />
-        <MovingInfluenceCard variant={"neutral"} />
-        <MovingInfluenceCard variant={"negative"} />
+        {impactData && (
+          <>
+            <MovingInfluenceCard
+              variant={Impacts.Positive}
+              data={impactData.data}
+            />
+            <MovingInfluenceCard
+              variant={Impacts.Neutral}
+              data={impactData.data}
+            />
+            <MovingInfluenceCard
+              variant={Impacts.Negative}
+              data={impactData.data}
+            />
+          </>
+        )}
       </div>
-      <Graph
-        variant={"bar"}
-        title={"Duur bewegingssessies voorbije week"}
-        className={styles.graph}
-        setShowModal={setShowModal}
-        setModalContent={setModalContent}
-      />
-      <Graph
-        variant={"line"}
-        title={"Pijn ervaring voorbije maand"}
-        className={clsx(styles.graph, styles.lastGraph)}
-        setShowModal={setShowModal}
-        setModalContent={setModalContent}
-      />
+      {movementData && painData && (
+        <>
+          <Graph
+            variant={"bar"}
+            title="Duur bewegingssessies voorbije week"
+            tooltip="Deze grafiek geeft de duur van de bewegingssessies weer per dag van de week uitgedrukt in minuten."
+            className={styles.graph}
+            setShowModal={setShowModal}
+            setModalContent={setModalContent}
+          ></Graph>
+          <Graph
+            variant={"line"}
+            title={"Pijn ervaring voorbije maand"}
+            data={fillMissingDates(painData.data.days)}
+            tooltip="Deze grafiek geeft de pijnervaring weer op een schaal van 0 tot 10."
+            className={clsx(styles.graph, styles.lastGraph)}
+            setShowModal={setShowModal}
+            setModalContent={setModalContent}
+          ></Graph>
+        </>
+      )}
       <TabBarNav />
     </ScrollableScreen>
   );
