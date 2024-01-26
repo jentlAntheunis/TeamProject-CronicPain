548ae8c605a828e274233eca7353ae80ca43ebe7
juul-vandevelde-howest <juul.van.de.velde@student.howest.be>
2024-01-26 17:15:28 +0100

    fix: removed console logs and the dummy data

--- app/src/components/screens/Specialist/QuestionnaireDetails/QuestionnaireDetails.jsx
+++ app/src/components/screens/Specialist/QuestionnaireDetails/QuestionnaireDetails.jsx
@@ -8,77 +8,6 @@ import { dateToDateTimeString } from "../../../../core/utils/timeData";
 import { useEffect } from "react";
 import { DatabaseCategories } from "../../../../core/config/questionCategories";
 
-const questions = [
-  {
-    question: "Hoe lang ervaart u al chronische pijn?",
-    answers: ["Minder dan 6 maanden", "6 maanden tot 1 jaar"],
-  },
-  {
-    question: "Welke behandelingen heeft u al geprobeerd?",
-    answers: ["Medicatie", "Fysiotherapie"],
-  },
-  {
-    question: "Hoe beïnvloedt chronische pijn uw dagelijks leven?",
-    answers: ["Ik kan niet meer werken", "Ik kan niet meer sporten"],
-  },
-  {
-    question: "Heeft u last van slaapproblemen door de pijn?",
-    answers: ["Ja", "Nee"],
-  },
-  {
-    question: "Welke medicijnen gebruikt u voor de pijnbestrijding?",
-    answers: ["Paracetamol", "Ibuprofen"],
-  },
-  {
-    question: "Heeft u al alternatieve therapieën geprobeerd?",
-    answers: ["Acupunctuur", "Massage"],
-  },
-  {
-    question: "Hoe gaat u om met de emotionele impact van chronische pijn?",
-    answers: [
-      "Ik heb last van depressieve gevoelens",
-      "Ik heb last van angstgevoelens",
-    ],
-  },
-  {
-    question:
-      "Heeft u ondersteuning van een zorgverlener voor uw pijnmanagement?",
-    answers: ["Ja", "Nee"],
-  },
-  {
-    question: "Wat zijn uw belangrijkste triggers voor pijnverergering?",
-    answers: ["Stress", "Beweging"],
-  },
-  {
-    question: "Hoe beïnvloedt chronische pijn uw werk of studie?",
-    answers: ["Ik kan niet meer werken", "Ik kan niet meer studeren"],
-  },
-  {
-    question: "Heeft u ervaring met mindfulness of ontspanningstechnieken?",
-    answers: ["Ja", "Nee"],
-  },
-  {
-    question: "Welke hulpmiddelen gebruikt u om met de pijn om te gaan?",
-    answers: ["Pijnmedicatie", "Ontspanningsoefeningen"],
-  },
-  {
-    question: "Heeft u last van sociale isolatie door de pijn?",
-    answers: ["Ja", "Nee"],
-  },
-  {
-    question:
-      "Hoe beïnvloedt chronische pijn uw stemming en mentale gezondheid?",
-    answers: [
-      "Ik heb last van depressieve gevoelens",
-      "Ik heb last van angstgevoelens",
-    ],
-  },
-  {
-    question: "Heeft u advies voor andere chronische pijnpatiënten?",
-    answers: ["Blijf bewegen", "Zoek hulp"],
-  },
-];
-
 const QuestionnaireDetails = () => {
   const { state } = useLocation();
   useEffect(() => {
@@ -104,7 +33,6 @@ const QuestionnaireDetails = () => {
         <div className={styles.questionContainer}>
           {state.questionnaire.questions.map(
             (question, index) => (
-              console.log(question, "question"),
               (
                 <div className={styles.question} key={index}>
                   <p className={styles.questionNumber}>Vraag {index + 1}</p>
@@ -121,7 +49,6 @@ const QuestionnaireDetails = () => {
                               ).optionContent
                             }
                           </p>
-                          {console.log(question.answers, "answers")}
                         </div>
                         <div>
                           <p className={styles.answerLabel}>Antwoord na:</p>
