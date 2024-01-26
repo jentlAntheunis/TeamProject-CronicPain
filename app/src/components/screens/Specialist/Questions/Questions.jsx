282813f7d8dd3182b8e050e84fa4983857f43eb0
Thibault <thibault.feraux@hotmail.com>
2024-01-26 09:34:12 +0100

    feat: refactor questions and set default filter on none

--- app/src/components/screens/Specialist/Questions/Questions.jsx
+++ app/src/components/screens/Specialist/Questions/Questions.jsx
@@ -9,73 +9,74 @@ import { SpecialistRoutes } from "../../../../core/config/routes";
 import { Plus } from "@phosphor-icons/react";
 import { useState } from "react";
 import clsx from "clsx";
+import QuestionList from "../../../app/questionnaire/QuestionList/QuestionList";
+
+const questions = [
+  {
+    category: "bewegingsvragen",
+    question: "Hoe lang ervaart u al chronische pijn?",
+  },
+  {
+    category: "bewegingsvragen",
+    question: "Welke behandelingen heeft u al geprobeerd?",
+  },
+  {
+    category: "bewegingsvragen",
+    question: "Hoe beïnvloedt chronische pijn uw dagelijks leven?",
+  },
+  {
+    category: "bewegingsvragen",
+    question: "Heeft u last van slaapproblemen door de pijn?",
+  },
+  {
+    category: "bewegingsvragen",
+    question: "Welke medicijnen gebruikt u voor de pijnbestrijding?",
+  },
+  {
+    category: "bewegingsvragen",
+    question: "Heeft u al alternatieve therapieën geprobeerd?",
+  },
+  {
+    category: "bewegingsvragen",
+    question: "Hoe gaat u om met de emotionele impact van chronische pijn?",
+  },
+  {
+    category: "bewegingsvragen",
+    question:
+      "Heeft u ondersteuning van een zorgverlener voor uw pijnmanagement?",
+  },
+  {
+    category: "bewegingsvragen",
+    question: "Wat zijn uw belangrijkste triggers voor pijnverergering?",
+  },
+  {
+    category: "bewegingsvragen",
+    question: "Hoe beïnvloedt chronische pijn uw werk of studie?",
+  },
+  {
+    category: "bewegingsvragen",
+    question: "Heeft u ervaring met mindfulness of ontspanningstechnieken?",
+  },
+  {
+    category: "bonusvragen",
+    question: "Welke hulpmiddelen gebruikt u om met de pijn om te gaan?",
+  },
+  {
+    category: "bonusvragen",
+    question: "Heeft u last van sociale isolatie door de pijn?",
+  },
+  {
+    category: "bonusvragen",
+    question:
+      "Hoe beïnvloedt chronische pijn uw stemming en mentale gezondheid?",
+  },
+  {
+    category: "bonusvragen",
+    question: "Heeft u advies voor andere chronische pijnpatiënten?",
+  },
+];
 
 const Questions = () => {
-  const questions = [
-    {
-      category: "bewegingsvragen",
-      question: "Hoe lang ervaart u al chronische pijn?",
-    },
-    {
-      category: "bewegingsvragen",
-      question: "Welke behandelingen heeft u al geprobeerd?",
-    },
-    {
-      category: "bewegingsvragen",
-      question: "Hoe beïnvloedt chronische pijn uw dagelijks leven?",
-    },
-    {
-      category: "bewegingsvragen",
-      question: "Heeft u last van slaapproblemen door de pijn?",
-    },
-    {
-      category: "bewegingsvragen",
-      question: "Welke medicijnen gebruikt u voor de pijnbestrijding?",
-    },
-    {
-      category: "bewegingsvragen",
-      question: "Heeft u al alternatieve therapieën geprobeerd?",
-    },
-    {
-      category: "bewegingsvragen",
-      question: "Hoe gaat u om met de emotionele impact van chronische pijn?",
-    },
-    {
-      category: "bewegingsvragen",
-      question:
-        "Heeft u ondersteuning van een zorgverlener voor uw pijnmanagement?",
-    },
-    {
-      category: "bewegingsvragen",
-      question: "Wat zijn uw belangrijkste triggers voor pijnverergering?",
-    },
-    {
-      category: "bewegingsvragen",
-      question: "Hoe beïnvloedt chronische pijn uw werk of studie?",
-    },
-    {
-      category: "bewegingsvragen",
-      question: "Heeft u ervaring met mindfulness of ontspanningstechnieken?",
-    },
-    {
-      category: "bonusvragen",
-      question: "Welke hulpmiddelen gebruikt u om met de pijn om te gaan?",
-    },
-    {
-      category: "bonusvragen",
-      question: "Heeft u last van sociale isolatie door de pijn?",
-    },
-    {
-      category: "bonusvragen",
-      question:
-        "Hoe beïnvloedt chronische pijn uw stemming en mentale gezondheid?",
-    },
-    {
-      category: "bonusvragen",
-      question: "Heeft u advies voor andere chronische pijnpatiënten?",
-    },
-  ];
-
   const [searchInput, setSearchInput] = useState("");
 
   const handleSearchChange = (event) => {
@@ -83,22 +84,15 @@ const Questions = () => {
   };
 
   // TODO: change to config categories
-  const [filters, setFilters] = useState(["bewegingsvragen", "bonusvragen"]);
+  const [filters, setFilters] = useState([]);
 
   const handleFilterClick = (filter) => {
     if (filters.includes(filter)) {
-        setFilters(filters.filter((f) => f !== filter));
+      setFilters(filters.filter((f) => f !== filter));
     } else {
       setFilters([...filters, filter]);
     }
   };
-
-  const filteredQuestions = questions
-    .filter((question) => filters.includes(question.category))
-    .filter((question) =>
-      question.question.toLowerCase().includes(searchInput.toLowerCase())
-    )
-    .map((question) => question.question);
 
   return (
     <ScrollableScreen>
@@ -138,40 +132,7 @@ const Questions = () => {
           </button>
         </div>
         <div className={styles.questions}>
-          {filteredQuestions.map((question, index) => {
-            const indexMatch = question
-              .toLowerCase()
-              .indexOf(searchInput.toLowerCase());
-            const nameBeforeMatch = question.slice(0, indexMatch);
-            const nameMatch = question.slice(
-              indexMatch,
-              indexMatch + searchInput.length
-            );
-            const nameAfterMatch = question.slice(
-              indexMatch + searchInput.length
-            );
-            return (
-              <div className={styles.question} key={index}>
-                <div className={styles.questionName}>
-                  {nameBeforeMatch}
-                  <span className={styles.match}>{nameMatch}</span>
-                  {nameAfterMatch}
-                </div>
-                <Button variant="tertiary" size="superSmall">
-                  Aanpassen
-                </Button>
-              </div>
-            );
-          })}
-          {filters.length === 0 ? (
-            <div className={styles.noQuestions}>
-              Selecteer een filter om vragen te zien
-            </div>
-          ) : filteredQuestions.length === 0 && (
-            <div className={styles.noQuestions}>
-              Geen vragen gevonden met deze zoekterm
-            </div>
-          )}
+          <QuestionList questions={questions} search={searchInput} filters={filters} />
         </div>
       </div>
     </ScrollableScreen>
